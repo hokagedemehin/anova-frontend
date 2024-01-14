8610079890cf67d490e1739f3d31b5daca530d07
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2024-01-14 18:33:07 +0000

    Commented out unused code in verify/page.tsx

--- app/(not_authenticated)/verify/page.tsx
+++ app/(not_authenticated)/verify/page.tsx
@@ -2,51 +2,51 @@
 import React, { useEffect } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Typography } from "@mui/material";
-import { useGoogleLogins } from "@/hooks/authHooks";
-import { setCookie } from "nookies";
+// import { useGoogleLogins } from "@/hooks/authHooks";
+// import { setCookie } from "nookies";
 import { enqueueSnackbar } from "notistack";
-import { AxiosError } from "axios";
+// import { AxiosError } from "axios";
 
 const VerifyLogin = () => {
   const searchParams = useSearchParams();
 
   const code = searchParams.get("code");
   console.log("code", code);
-  const googleMutation = useGoogleLogins();
+  // const googleMutation = useGoogleLogins();
   const router = useRouter();
   useEffect(() => {
     if (code) {
-      const form_data = { code };
+      // const form_data = { code };
       try {
-        googleMutation.mutate(form_data, {
-          onSuccess: (data: any) => {
-            enqueueSnackbar("Login successfully", { variant: "success" });
-            setCookie(null, "anova_token", data?.data?.key, {
-              // path: "/",
-              maxAge: 30 * 24 * 60 * 60,
-              sameSite: "lax",
-            });
-            router.push("/bids");
-            window.location.reload();
-          },
-          onError: (error) => {
-            if (error instanceof AxiosError) {
-              if (error?.response?.data?.non_field_errors) {
-                enqueueSnackbar(error?.response?.data?.non_field_errors[0], {
-                  variant: "error",
-                });
-              } else {
-                enqueueSnackbar(
-                  "Something went wrong, please try again later.",
-                  {
-                    variant: "error",
-                  },
-                );
-              }
-            }
-            router.push("/login");
-          },
-        });
+        // googleMutation.mutate(form_data, {
+        //   onSuccess: (data: any) => {
+        //     enqueueSnackbar("Login successfully", { variant: "success" });
+        //     setCookie(null, "anova_token", data?.data?.key, {
+        //       // path: "/",
+        //       maxAge: 30 * 24 * 60 * 60,
+        //       sameSite: "lax",
+        //     });
+        //     router.push("/bids");
+        //     window.location.reload();
+        //   },
+        //   onError: (error) => {
+        //     if (error instanceof AxiosError) {
+        //       if (error?.response?.data?.non_field_errors) {
+        //         enqueueSnackbar(error?.response?.data?.non_field_errors[0], {
+        //           variant: "error",
+        //         });
+        //       } else {
+        //         enqueueSnackbar(
+        //           "Something went wrong, please try again later.",
+        //           {
+        //             variant: "error",
+        //           },
+        //         );
+        //       }
+        //     }
+        //     router.push("/login");
+        //   },
+        // });
       } catch (error) {
         enqueueSnackbar("Something went wrong, please try again later.", {
           variant: "error",
